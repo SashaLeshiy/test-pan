7963820002b414265fe8b450576b9b59eb5c3c47
SashaLeshiy <zomlesh@yandex.ru>
2021-11-23 11:01:19 +0300

    редирект для netlify

--- src/components/TableRow/TableRow.js
+++ src/components/TableRow/TableRow.js
@@ -17,28 +17,28 @@ function TableRow({ user, stateUser, setStateUser, _id }) {
             <td><textarea
                 name="text"
                 value={user.data.text}
-                className="form-control"
+                className="form-control tableRow"
                 type="text"
                 onChange={addData}
             /></td>
             <td><input
                 name="fname"
                 value={user.data.fname}
-                className="form-control"
+                className="form-control tableRow"
                 type="text"
                 onChange={addData}
             /></td>
             <td><input
                 name="lname"
                 value={user.data.lname}
-                className="form-control"
+                className="form-control tableRow"
                 type="text"
                 onChange={addData}
             /></td>
             <td><input
                 name="email"
                 value={user.data.email}
-                className="form-control"
+                className="form-control tableRow"
                 type="email"
                 onChange={addData}
             /></td>
